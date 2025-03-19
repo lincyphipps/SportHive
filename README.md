b9bb944e9910b17927420cbf398e3f4226af7324
cosmicatta <myiah.stubbs@ufl.edu>
2025-03-19 02:10:34 -0400

    added post+user models and user auth routes. changed readme

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # SportHive
 Run these (each is one line in the terminal):
 1. git clone  https://github.com/lincyphipps/SportHive.git
-2. npm install express mongoose cors dotenv
+2. npm install express mongoose cors dotenv mongodb bcrypt
 3. npm install --save-dev nodemon
-4. npm install (idk if this one is necesary)
-5. npm install mongodb
-6. 
-
---
+++ backend/models/Posts.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Placeholder post schema that can be changed later
+const postschema = new Schema({
+  title: {
+    type: String, 
+    required: true
+  },
+
+  author: {
+    type: Schema.Types.ObjectId, ref: "User",
+    required: true
+  },
+
+  text: {
+    type: String, 
+    required: true
+  },
+});
+
+module.exports = mongoose.model("Post", postschema);
